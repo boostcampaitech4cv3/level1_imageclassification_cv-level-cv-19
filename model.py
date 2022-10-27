28c0b751603080ffcdc87a3aa7615c2b8a096e76
jerry-ryu <fbrjs15@naver.com>
2022-10-27 17:16:01 +0000

    add resnet50

--- model.py
+++ model.py
@@ -33,20 +33,21 @@ class BaseModel(nn.Module):
         return self.fc(x)
 
 
-# Custom Model Template
-class MyModel(nn.Module):
-    def __init__(self, num_classes):
+# resnet 50
+from torchvision.models import resnet50
+class ResNet50(nn.Module):
+    def __init__(self):
         super().__init__()
 
-        """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
-        """
+        self.backbone = resnet50(pretrained=True)
+        self.backbone.fc = nn.Linear(2048,18)
+        
+        # freeze except classifier
+        for parameter in self.backbone.parameters():
+            parameter.requires_grad = False
+        self.backbone.fc.weight.requires_grad = True
+        self.backbone.fc.bias.requires_grad = True
 
     def forward(self, x):
-        """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
-        """
-        return x
+        out = self.backbone(x)
+        return out
