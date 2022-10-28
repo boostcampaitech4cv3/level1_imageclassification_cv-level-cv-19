6bd8a3e4f27682739bef6a51c3844a64d9e5e859
Gu Lee <99sphere@gmail.com>
2022-10-28 08:48:07 +0000

    feat : Implementation of CutMix function in train.py and minor update in multi_head_inference.py

--- multi_head_train.py
+++ multi_head_train.py
@@ -286,9 +286,7 @@ def train(data_dir, model_dir, args):
                 gender_labels = gender_labels.to(device)
                 age_labels = age_labels.to(device)
 
-
-
-
+                r = np.random.rand(1)
                 if args.beta > 0 and r < args.cutmix_prob:
                     # generate mixed sample
                     lam = np.random.beta(args.beta, args.beta)
