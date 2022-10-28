668742b67cac4293ba52a490c8f2b8d238d9eade
Gu Lee <99sphere@gmail.com>
2022-10-28 02:14:32 +0000

    feat: Implementation of train and inference code for Multi-Head Model

---
+++ multi_head_inference.py
@@ -0,0 +1,96 @@
+import argparse
+import multiprocessing
+import os
+from importlib import import_module
+
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+
+from dataset import TestDataset, MaskBaseDataset
+
+def encode_multi_class(mask_label, gender_label, age_label) -> int:
+    return mask_label * 6 + gender_label * 3 + age_label
+
+
+def load_model(saved_model, device):
+    model_cls = getattr(import_module("model"), args.model)
+    model = model_cls(
+    )
+
+    # tarpath = os.path.join(saved_model, 'best.tar.gz')
+    # tar = tarfile.open(tarpath, 'r:gz')
+    # tar.extractall(path=saved_model)
+
+    model_path = os.path.join(saved_model, 'best.pth')
+    model.load_state_dict(torch.load(model_path, map_location=device))
+
+    return model
+
+
+@torch.no_grad()
+def inference(data_dir, model_dir, output_dir, args):
+    """
+    """
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    model = load_model(model_dir, device).to(device)
+    model.eval()
+
+    img_root = os.path.join(data_dir, 'images')
+    info_path = os.path.join(data_dir, 'info.csv')
+    info = pd.read_csv(info_path)
+
+    img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+    dataset = TestDataset(img_paths, args.resize)
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        num_workers=multiprocessing.cpu_count() // 2,
+        shuffle=False,
+        pin_memory=use_cuda,
+        drop_last=False,
+    )
+
+    print("Calculating inference results..")
+    preds = []
+    with torch.no_grad():
+        for idx, images in enumerate(loader):
+            images = images.to(device)
+            mask, gender, age = model(images) # shape: B * 3
+            pred_mask = mask.argmax(dim=-1) # shape: B * 1
+            pred_gender = gender.argmax(dim=-1) # shape: B * 1
+            pred_age = age.argmax(dim=-1) # shape: B * 1
+
+            pred = encode_multi_class(pred_mask, pred_gender, pred_age)
+            preds.extend(pred.cpu().numpy())
+
+    info['ans'] = preds
+    save_path = os.path.join(output_dir, f'output.csv')
+    info.to_csv(save_path, index=False)
+    print(f"Inference Done! Inference result saved at {save_path}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Data and model checkpoints directories
+    parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--resize', type=tuple, default=(96, 128), help='resize size for image when you trained (default: (96, 128))')
+    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+
+    # Container environment
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_CHANNEL_MODEL', './model/exp'))
+    parser.add_argument('--output_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR', './output'))
+
+    args = parser.parse_args()
+
+    data_dir = args.data_dir
+    model_dir = args.model_dir
+    output_dir = args.output_dir
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    inference(data_dir, model_dir, output_dir, args)
