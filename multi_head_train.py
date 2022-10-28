be8bf1bc083718d29b60bc50d79ae7d6cc8e9469
Gu Lee <99sphere@gmail.com>
2022-10-28 05:11:37 +0000

    fix : Fix error abour early stopping argument

--- multi_head_train.py
+++ multi_head_train.py
@@ -310,6 +310,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
     parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
     parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
+    parser.add_argument('--patient', default = 15, help='early stopping patient(default: 15)')
 
     # Container environment
     parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
