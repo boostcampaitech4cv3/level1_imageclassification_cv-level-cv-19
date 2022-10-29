71d0ac9e3cb6c60ad74bca7a2ea85424847845b6
GH < jane79.bn@gmail.com>
2022-10-29 08:06:36 +0000

    feat : DenseNet121

--- model.py
+++ model.py
@@ -161,10 +161,12 @@ class DenseNet121(nn.Module):
         for parameter in self.backbone.parameters():
             parameter.requires_grad = False
         self.backbone.classifier.weight.requires_grad = True
-        self.bn1 = nn.BatchNorm2d(3)
+        self.bn1 = nn.BatchNorm1d(512)
         self.classifier2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm2d(3)
-        self.classifier3 = nn.Linear(256, 18)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.classifier3 = nn.Linear(256, 128)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.classifier4 = nn.Linear(128, 18)
         self.relu = nn.ReLU()
     
     def forward(self, x):
@@ -174,14 +176,17 @@ class DenseNet121(nn.Module):
         x = self.classifier2(x)
         x = self.relu(x)
         x = self.bn2(x)
-        output = self.classifir3(x)
+        x = self.classifier3(x)
+        x = self.relu(x)
+        x = self.bn3(x)
+        output = self.classifier4(x)
         return output
     
 # densenet201
 class DenseNet201(nn.Module):
     def __init__(self, num_classes = 18):
         super().__init__()
-        self.backbone = torch.hub.load('pytorch/vision:v0.10.0', 'densenet201', pretrained=True)
+        self.backbone = torch.hub.load('pytorch/vision:v0.8.2', 'densenet201', pretrained=True)
         self.classifier = nn.Linear(1920,num_classes)
     
     def forward(self, x):
