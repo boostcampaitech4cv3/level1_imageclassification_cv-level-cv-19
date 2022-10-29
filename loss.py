14eace808bc7c40c3bc35de780c78282c77a9059
jerry-ryu <fbrjs15@naver.com>
2022-10-29 14:14:35 +0000

    feat: add focal loss with label smoothing

--- loss.py
+++ loss.py
@@ -24,7 +24,7 @@ class FocalLoss(nn.Module):
 
 
 class LabelSmoothingLoss(nn.Module):
-    def __init__(self, classes=3, smoothing=0.0, dim=-1):
+    def __init__(self, classes=18, smoothing=0.2, dim=-1):
         super(LabelSmoothingLoss, self).__init__()
         self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
@@ -42,7 +42,7 @@ class LabelSmoothingLoss(nn.Module):
 
 # https://gist.github.com/SuperShinyEyes/dcc68a08ff8b615442e3bc6a9b55a354
 class F1Loss(nn.Module):
-    def __init__(self, classes=3, epsilon=1e-7):
+    def __init__(self, classes=18, epsilon=1e-7):
         super().__init__()
         self.classes = classes
         self.epsilon = epsilon
@@ -64,13 +64,78 @@ class F1Loss(nn.Module):
         f1 = 2 * (precision * recall) / (precision + recall + self.epsilon)
         f1 = f1.clamp(min=self.epsilon, max=1 - self.epsilon)
         return 1 - f1.mean()
+    
+    
+class FocalLossWithSmoothing(nn.Module):
+    def __init__(
+            self,
+            num_classes: int = 18,
+            gamma: int = 2,
+            lb_smooth: float = 0.2,
+            ignore_index: int = None
+            ):
+        """
+        :param gamma:
+        :param lb_smooth:
+        :param ignore_index:
+        """
+        super(FocalLossWithSmoothing, self).__init__()
+        self._num_classes = num_classes
+        self._gamma = gamma
+        self._lb_smooth = lb_smooth
+        self._ignore_index = ignore_index
+        self._log_softmax = nn.LogSoftmax(dim=1)
+
+        if self._num_classes <= 1:
+            raise ValueError('The number of classes must be 2 or higher')
+        if self._gamma < 0:
+            raise ValueError('Gamma must be 0 or higher')
+
+    def forward(self, logits, label):
+        """
+        :param logits: (batch_size, class, height, width)
+        :param label:
+        :return:
+        """
+        logits = logits.float()
+        difficulty_level = self._estimate_difficulty_level(logits, label)
+
+        with torch.no_grad():
+            label = label.clone().detach()
+            if self._ignore_index is not None:
+                ignore = label.eq(self._ignore_index)
+                label[ignore] = 0
+            lb_pos, lb_neg = 1. - self._lb_smooth, self._lb_smooth / (self._num_classes - 1)
+            lb_one_hot = torch.empty_like(logits).fill_(
+                lb_neg).scatter_(1, label.unsqueeze(1), lb_pos).detach()
+        logs = self._log_softmax(logits)
+        loss = -torch.sum(difficulty_level * logs * lb_one_hot, dim=1)
+        if self._ignore_index is not None:
+            loss[ignore] = 0
+        return loss.mean()
+
+    def _estimate_difficulty_level(self, logits, label):
+        """
+        :param logits:
+        :param label:
+        :return:
+        """
+        one_hot_key = torch.nn.functional.one_hot(label, num_classes=self._num_classes)
+        if len(one_hot_key.shape) == 4:
+            one_hot_key = one_hot_key.permute(0, 3, 1, 2)
+        if one_hot_key.device != logits.device:
+            one_hot_key = one_hot_key.to(logits.device)
+        pt = one_hot_key * F.softmax(logits)
+        difficulty_level = torch.pow(1 - pt, self._gamma)
+        return difficulty_level
 
 
 _criterion_entrypoints = {
     'cross_entropy': nn.CrossEntropyLoss,
     'focal': FocalLoss,
     'label_smoothing': LabelSmoothingLoss,
-    'f1': F1Loss
+    'f1': F1Loss,
+    'focal_with_ls':FocalLossWithSmoothing
 }
 
 
