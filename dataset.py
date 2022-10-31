fb97301e12e7b927beb93515a25b24c1c1e70a71
jerry-ryu <fbrjs15@naver.com>
2022-10-31 02:34:03 +0000

    feat: edit testdatset augmentation and add dataset args

--- dataset.py
+++ dataset.py
@@ -383,6 +383,7 @@ class TestDataset(Dataset):
     def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
         self.img_paths = img_paths
         self.transform = Compose([
+            CenterCrop((320, 256)),
             Resize(resize, Image.BILINEAR),
             ToTensor(),
             Normalize(mean=mean, std=std),
