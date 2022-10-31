71a9c8c2f066ae89fd20b95bc5aedd04958c47f0
jerry-ryu <fbrjs15@naver.com>
2022-10-31 13:45:45 +0000

    feat: add EfficientNet_b0

--- model.py
+++ model.py
@@ -1,6 +1,29 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torchvision.models import efficientnet_b0
+class EfficientNet_B0(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.backbone = efficientnet_b0(weights='DEFAULT')
+        self.backbone.classifier = nn.Sequential(
+            nn.Linear(1280, 1024),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(1024),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(512),
+            nn.Linear(512, 128),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(128),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        output = self.backbone(x)
+        return output
+
+
 from torchvision.models import efficientnet_b2
 class EfficientNet_B2(nn.Module):
     def __init__(self, num_classes):
