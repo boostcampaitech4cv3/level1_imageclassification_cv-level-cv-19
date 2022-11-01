282d6c75bb3da6acc73121184ed9019ee39ea372
jerry-ryu <fbrjs15@naver.com>
2022-11-01 17:12:36 +0000

    feat: add stratifieddataset

--- dataset.py
+++ dataset.py
@@ -266,6 +266,7 @@ class MaskBaseDataset(Dataset):
         "mask3": MaskLabels.MASK,
         "mask4": MaskLabels.MASK,
         "mask5": MaskLabels.MASK,
+        "mask6": MaskLabels.MASK,
         "incorrect_mask": MaskLabels.INCORRECT,
         "normal": MaskLabels.NORMAL
     }
@@ -386,6 +387,107 @@ class MaskBaseDataset(Dataset):
         train_set, val_set = random_split(self, [n_train, n_val])
         return train_set, val_set
 
+import pandas as pd
+class StratifiedProfileDataset(MaskBaseDataset):
+    """
+        train / val 나누는 기준을 이미지에 대해서 random 이 아닌
+        사람(profile)을 기준으로 나눕니다.
+        구현은 val_ratio 에 맞게 train / val 나누는 것을 이미지 전체가 아닌 사람(profile)에 대해서 진행하여 indexing 을 합니다
+        이후 `split_dataset` 에서 index 에 맞게 Subset 으로 dataset 을 분기합니다.
+    """
+
+    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
+        self.indices = defaultdict(list)
+        self.multi_labels = []
+        super().__init__(data_dir, mean, std, val_ratio)
+
+    @staticmethod
+    def _split_profile(profiles, val_ratio):
+        length = len(profiles)
+        n_val = int(length * val_ratio)
+        
+        df = pd.read_csv("/opt/ml/input/data/train/train_data.csv")
+        class_list = df[["profile"]].values.tolist()
+        
+        man_young = []
+        man_normal = []
+        man_old = []
+        female_young = []
+        female_normal = []
+        female_old = []
+        
+        for idx, profile in enumerate(class_list):
+            profile = profile[0]
+            if profile == 612:
+                man_young.append(idx)
+            elif profile == 1713:
+                man_normal.append(idx)
+            elif profile == 2814:
+                man_old.append(idx)
+            elif profile == 3915:
+                female_young.append(idx)
+            elif profile == 41016:
+                female_normal.append(idx)
+            elif profile == 51117:
+                female_normal.append(idx)
+                
+                
+        val_indices = set()
+        
+        for class_type in [man_young, man_normal, man_old, female_young, female_normal, female_old]:
+                #val_indices.update(set(random.sample(class_type, k=n_val)))
+                tmp = pd.Series(class_type)
+                tmp = tmp.sample(frac = val_ratio)
+                tmp = tmp.tolist()
+                val_indices.update(set(tmp))
+                
+        train_indices = set(range(length)) - val_indices
+        return {
+            "train": train_indices,
+            "val": val_indices
+        }
+
+    def setup(self):
+        profiles = os.listdir(self.data_dir)
+        profiles = [profile for profile in profiles if not profile.startswith(".")]
+        split_profiles = self._split_profile(profiles, self.val_ratio)
+
+        cnt = 0
+        for phase, indices in split_profiles.items():
+            for _idx in indices:
+                profile = profiles[_idx]
+                img_folder = os.path.join(self.data_dir, profile)
+                for file_name in os.listdir(img_folder):
+                    _file_name, ext = os.path.splitext(file_name)
+                    if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                        continue
+
+                    img_path = os.path.join(self.data_dir, profile, file_name)  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                    mask_label = self._file_names[_file_name]
+
+                    id, gender, race, age = profile.split("_")
+                    gender_label = GenderLabels.from_str(gender)
+                    age_label = AgeLabels.from_number(age)
+
+                    self.image_paths.append(img_path)
+                    self.mask_labels.append(mask_label)
+                    self.gender_labels.append(gender_label)
+                    self.age_labels.append(age_label)
+                    self.indices[phase].append(cnt)
+                    cnt += 1
+                    if phase == "train":
+                        self.multi_labels.append(self.encode_multi_class(mask_label, gender_label, age_label))
+
+    def split_dataset(self) -> List[Subset]:
+        return [Subset(self, indices) for phase, indices in self.indices.items()]
+    
+    def get_multi_labels(self):
+        return self.multi_labels
+    
+
+a = StratifiedProfileDataset("/opt/ml/input/data/train/images")
+    
+    
 
 class MaskSplitByProfileDataset(MaskBaseDataset):
     """
@@ -448,6 +550,10 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
     
     def get_multi_labels(self):
         return self.multi_labels
+        
+    
+    
+
         
 
 # 3-body dataset
